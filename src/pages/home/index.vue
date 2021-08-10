1c2e2dca2f519b127592946d8e591f8d5425012d
= <2875802186@qq.com>
2021-08-10 18:00:19 +0800

    添加动态id并完成首页

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -12,12 +12,13 @@
         v-model="value"
         placeholder="请输入搜索关键词"
         class="topipt"
+        @search="onSearch"
       />
       <van-icon name="friends-o" color="#999" class="toprig" />
     </div>
 
     <!-- nav -->
-    <van-tabs v-model="active" sticky class="nav">
+    <van-tabs v-model="active" @click="classify" sticky class="nav">
       <van-tab v-for="item in topnav" :title="item.title" :key="item.id">
         <!-- 内容 {{ item }} -->
       </van-tab>
@@ -40,19 +41,27 @@
       />
     </van-grid>
     <!-- 商品列表 -->
-    <div>
-      <p><img src="" alt="" /></p>
-      <p>
-        <span>商品名</span>
-        <span>商品介绍</span>
-        <span>价格</span>
-        <button>立即购买</button>
-      </p>
-    </div>
+    <van-list
+      v-model="loading"
+      :finished="finished"
+      finished-text="没有更多了"
+      @load="onLoad"
+      class="lanlist"
+    >
+      <li v-for="item in productlist" :key="item._id" class="productlist">
+        <p class="productimg"><img :src="item.coverImg" alt="" /></p>
+        <p class="productBtm">
+          <span>商品名{{ item.name }}</span>
+          <span class="liPrice">价格{{ item.price }}</span>
+          <button class="btn">立即购买</button>
+        </p>
+      </li>
+    </van-list>
   </div>
 </template>
 
 <script>
+import { get } from "../../utils/request";
 export default {
   components: {},
   data() {
@@ -60,17 +69,17 @@ export default {
       value: "",
       active: 0,
       topnav: [
-        { id: 1, title: "推荐" },
-        { id: 2, title: "手机" },
-        { id: 3, title: "智能" },
-        { id: 4, title: "电视" },
-        { id: 5, title: "家电" },
-        { id: 6, title: "笔记本" },
+        { id: 1, title: "推荐", name: "五五" },
+        { id: 2, title: "手机", name: "手机" },
+        { id: 3, title: "手表", name: "智能手表" },
+        { id: 4, title: "电视", name: "电视" },
+        { id: 5, title: "口红", name: "口红" },
+        { id: 6, title: "耳机", name: "耳机" },
       ],
       toplist: [
         { id: 1, title: "米金商城", icon: "gem-o" },
         { id: 2, title: "小米众筹", icon: "gift-o" },
-        { id: 3, title: "Note9系列", icon: "paid" },
+        { id: 3, title: "Note9", icon: "paid" },
         { id: 4, title: "以旧换新", icon: "discount" },
         { id: 5, title: "小米上新", icon: "brush-o" },
         { id: 6, title: "智能", icon: "smile-o" },
@@ -96,6 +105,9 @@ export default {
             "https://img2.baidu.com/it/u=624006919,3231102418&fm=26&fmt=auto&gp=0.jpg",
         },
       ],
+      productlist: [],
+      loading: false,
+      finished: false,
     };
   },
   computed: {},
@@ -104,14 +116,57 @@ export default {
   methods: {
     //   获取推荐页面商品信息
     async getproductlist() {
+      // 加载状态结束
+      this.loading = true;
       const result = await get("/api/v1/products", {
-        per: 10,
+        per: 120,
         page: 1,
       });
       console.log(result);
+      this.loading = false;
+      this.productlist = [...this.productlist, ...result.data.products];
+      console.log(this.productlist);
+      if (result.data.products.length < 10) {
+        this.finished = true;
+      } else if (result.data.products.length >= 120) {
+        this.finished = true;
+      } else {
+        this.finished = false;
+      }
+    },
+
+    // 懒加载
+    onLoad() {
+      this.getproductlist();
+    },
+
+    // 搜索框
+    onSearch(val) {
+      var seach = [];
+      this.productlist.forEach((item) => {
+        if (item.name.includes(val)) {
+          seach.push(item);
+        }
+        this.productlist = seach;
+      });
+    },
+    // 分类
+    async classify() {
+      const result = await get("/api/v1/products", {
+        name: this.topnav[this.active].name,
+      });
+      console.log(result.data);
+      //   console.log(result.data.products);
+      this.productlist = result.data.products;
+      if (this.active == 0) {
+        this.getproductlist();
+      }
     },
   },
-  created() {},
+
+  created() {
+    this.getproductlist();
+  },
   mounted() {},
 };
 </script>
@@ -156,5 +211,36 @@ export default {
 .van-swipe-item img {
   width: 100%;
   height: 180px;
+}
+/* 商品列表 */
+
+.productlist {
+  display: inline-block;
+  width: 45%;
+  text-align: center;
+  margin: 5px 9px;
+  background: rgb(246, 246, 246);
+}
+.productBtm {
+  display: flex;
+  flex-direction: column;
+}
+.productimg img {
+  width: 150px;
+  height: 150px;
+}
+.btn {
+  width: 100px;
+  height: 30px;
+  color: #fff;
+  border: none;
+  background-color: rgb(255, 128, 0);
+  margin: 0 auto;
+}
+.lanlist {
+  padding-bottom: 50px;
+}
+.liPrice {
+  margin: 8px auto;
 }
 </style>
