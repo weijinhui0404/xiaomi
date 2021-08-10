7ac7bedfed59829878f3c2035bbfdcc041853386
chenwanli <2023257633@qq.com>
2021-08-10 10:22:39 +0800

    拉取axios

--- src/pages/fenlei/index.vue
+++ src/pages/fenlei/index.vue
@@ -1,31 +1,30 @@
 <template>
-    <div class='fenlei'>分类页面</div>
+  <div class="fenlei">
+    <van-sidebar v-model="activeKey">
+      <van-sidebar-item title="口红" @click="getkouhong" />
+      <van-sidebar-item title="手机" />
+      <van-sidebar-item title="智能手表" />
+      <van-sidebar-item title="耳机" />
+      <van-sidebar-item title="电视" />
+      <van-sidebar-item title="灯具" />
+    </van-sidebar>
+  </div>
 </template>
 
 <script>
 export default {
-    
-    components: {},
-    data() {
-        
-        return {
-            
-        };
-    },
-    computed: {},
-    watch: {},
-    
-    methods: {
-        
-    },
-    created() {
-        
-    },
-    mounted() {
-        
-    },
-    }
+  components: {},
+  data() {
+    return {
+      activeKey: 0,
+    };
+  },
+  computed: {},
+  watch: {},
+
+  methods: {},
+  created() {},
+  mounted() {},
+};
 </script>
-<style scoped>
-    
-</style>
+<style scoped></style>
