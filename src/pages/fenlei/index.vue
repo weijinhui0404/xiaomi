6c8b15a4f1f7b37894a81d7301e622bd78cbb21b
chenwanli <2023257633@qq.com>
2021-08-10 12:59:55 +0800

    完成分类功能

--- src/pages/fenlei/index.vue
+++ src/pages/fenlei/index.vue
@@ -1,30 +1,103 @@
 <template>
   <div class="fenlei">
-    <van-sidebar v-model="activeKey">
-      <van-sidebar-item title="口红" @click="getkouhong" />
-      <van-sidebar-item title="手机" />
-      <van-sidebar-item title="智能手表" />
-      <van-sidebar-item title="耳机" />
-      <van-sidebar-item title="电视" />
-      <van-sidebar-item title="灯具" />
+    <van-sidebar v-model="activeKey" class="sti">
+      <van-sidebar-item title="推荐" @click="getalllist" />
+      <van-sidebar-item
+        v-for="(item, index) in titlist"
+        :title="item.name"
+        :key="item._id"
+        @click="getlist(index)"
+      />
     </van-sidebar>
+    <div class="div">
+      <van-list
+        v-model="loading"
+        :finished="finished"
+        finished-text="没有更多了"
+        @load="onLoad"
+        :offset="50"
+      >
+        <van-card
+          v-for="(item, index) in list"
+          :key="item._id + index"
+          :price="item.price"
+          :title="item.name"
+          :thumb="item.coverImg"
+        />
+      </van-list>
+    </div>
   </div>
 </template>
 
 <script>
+import { get } from "../../utils/request";
 export default {
   components: {},
   data() {
     return {
       activeKey: 0,
+      list: [],
+      titlist: [],
+      loading: false,
+      finished: false,
     };
   },
   computed: {},
   watch: {},
 
-  methods: {},
-  created() {},
+  methods: {
+    onLoad() {},
+    //   推荐列表   我这里是显示了全部
+    async getalllist() {
+      this.loading = true;
+      const res = await get("/api/v1/products", {
+        per: 120,
+        page: 1,
+      });
+      this.loading = false;
+      this.list = [...this.list, ...res.data.products];
+      if (this.list.length < 10) {
+        this.finished = true;
+      } else if (this.list.length >= 120) {
+        this.finished = true;
+      } else {
+        this.finished = false;
+      }
+    },
+    // 分类传入不同的 参数
+    async getlist(i) {
+      const res = await get("/api/v1/products", {
+        name: this.titlist[i].name,
+      });
+      //   console.log(res);
+      this.list = res.data.products;
+    },
+    // 获取分类 api
+    async getmmm() {
+      const res = await get("/api/v1/product_categories");
+      //   console.log(res);
+      this.titlist = res.data.categories;
+    },
+  },
+  created() {
+    //   开始就显示 推荐页面
+    this.getalllist();
+    this.getmmm();
+  },
   mounted() {},
 };
 </script>
-<style scoped></style>
+<style scoped>
+.sti{
+    position: fixed;
+    left: 0;
+    top: 0;
+    float: left;
+}
+.div{
+    width: 75%;
+    height: 100%;
+    float: right;
+    padding-bottom: 60px;
+}
+</style>
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,28 +1,33 @@
 import axios from "axios";
-import {BASE_URL}  from "./tools"
-
+import { BASE_URL } from "./tools";
+import { getToken } from "./auth";
 const instance = axios.create({
-    baseURL:BASE_URL,
-    tiemout:5000,
-})
-instance.interceptors.request.use(function (config) {
+  baseURL: BASE_URL,
+  tiemout: 5000,
+});
+instance.interceptors.request.use(
+  function(config) {
     //   在请求拦截里面统一设置header头
     if (getToken()) {
       config.headers.authorization = "Bearer " + getToken();
     }
     return config;
-  }, function (error) {
+  },
+  function(error) {
     return Promise.reject(error);
-  });
-  
-  
-  instance.interceptors.response.use(function (response) {
+  }
+);
+
+instance.interceptors.response.use(
+  function(response) {
     return response;
-  }, function (error) {
+  },
+  function(error) {
     return Promise.reject(error);
-  });
-  
-  export const get = (url, params) => instance.get(url, { params })
-  export const post = (url, data) => instance.post(url, data)
-  export const delet = (url, params) => instance.delete(url, { params })
-  export default instance;
+  }
+);
+
+export const get = (url, params) => instance.get(url, { params });
+export const post = (url, data) => instance.post(url, data);
+export const delet = (url, params) => instance.delete(url, { params });
+export default instance;
