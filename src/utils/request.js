273bcf24d9a491fde2ae962a8de6b2f154df984a
Colin <1782568785@qq.com>
2021-08-12 12:27:42 +0800

    添加了按钮

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,28 +1,32 @@
 import axios from "axios";
-import { BASE_URL } from "./tools";
-import { getToken } from "./auth";
+import {
+  BASE_URL
+} from "./tools";
+import {
+  getToken
+} from "./auth";
 const instance = axios.create({
   baseURL: BASE_URL,
   tiemout: 5000,
 });
 instance.interceptors.request.use(
-  function(config) {
+  function (config) {
     //   在请求拦截里面统一设置header头
     if (getToken()) {
       config.headers.authorization = "Bearer " + getToken();
     }
     return config;
   },
-  function(error) {
+  function (error) {
     return Promise.reject(error);
   }
 );
 
 instance.interceptors.response.use(
-  function(response) {
+  function (response) {
     return response;
   },
-  function(error) {
+  function (error) {
     return Promise.reject(error);
   }
 );
@@ -32,6 +36,10 @@ export const get = (url, params) =>
     params,
   });
 export const post = (url, data) => instance.post(url, data);
-export const delet = (url, params) => instance.delete(url, { params });
-export const put = (url, params) => instance.put(url, { params });
+export const delet = (url, params) => instance.delete(url, {
+  params
+});
+export const put = (url, params) => instance.put(url, {
+  params
+});
 export default instance;
